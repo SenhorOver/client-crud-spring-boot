aa472afc9339f1d67b4ac05cbbd1eac6fe93f21a
Marcos V Silva <mrcsvs@outlook.com>
2025-07-14 18:50:22 -0300

    POST products + import.sql FIX

--- src/main/java/com/srover/client/services/ClientService.java
+++ src/main/java/com/srover/client/services/ClientService.java
@@ -8,19 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
         Client entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ClientDTO(entity);
     }
 
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAll(Pageable pageable) {
         Page<Client> entities = repository.findAll(pageable);
         return entities.map(ClientDTO::new);
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
     }
 }

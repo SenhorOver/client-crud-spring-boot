fb61ea5eab33c071a346e8d8247fc204ab216635
Marcos V Silva <mrcsvs@outlook.com>
2025-07-14 18:31:03 -0300

    GET products pageable

--- src/main/java/com/srover/client/controllers/ClientController.java
+++ src/main/java/com/srover/client/controllers/ClientController.java
@@ -3,11 +3,15 @@ package com.srover.client.controllers;
 import com.srover.client.dto.ClientDTO;
 import com.srover.client.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/client")
@@ -20,5 +24,11 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dtos = service.findAll(pageable);
+        return ResponseEntity.ok(dtos);
     }
 }
--- src/main/java/com/srover/client/services/ClientService.java
+++ src/main/java/com/srover/client/services/ClientService.java
@@ -5,6 +5,8 @@ import com.srover.client.entities.Client;
 import com.srover.client.repositories.ClientRepository;
 import com.srover.client.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +17,10 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ClientDTO(entity);
+    }
+
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> entities = repository.findAll(pageable);
+        return entities.map(ClientDTO::new);
     }
 }

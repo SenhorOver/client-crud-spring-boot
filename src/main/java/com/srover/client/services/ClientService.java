e86f53aaca7f742cdbdecd4809de7cb108843747
Marcos V Silva <mrcsvs@outlook.com>
2025-07-15 23:01:19 -0300

    PUT client

--- src/main/java/com/srover/client/services/ClientService.java
+++ src/main/java/com/srover/client/services/ClientService.java
@@ -30,13 +30,27 @@ public class ClientService {
     @Transactional
     public ClientDTO insert(ClientDTO dto) {
         Client entity = new Client();
+        copyToEntity(dto, entity);
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        Client entity = repository.getReferenceById(id);
+
+        copyToEntity(dto, entity);
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void copyToEntity(ClientDTO dto, Client entity) {
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
         entity.setIncome(dto.getIncome());
         entity.setBirthDate(dto.getBirthDate());
         entity.setChildren(dto.getChildren());
-
-        entity = repository.save(entity);
-        return new ClientDTO(entity);
     }
 }
